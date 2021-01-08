774843488fbc42d5bc06d2cdd976b1689b34cbb5
ProRobotnic(Frodo) <shuvalov-04@inbox.ru>
2021-01-08 18:41:19 +0300

    Def for drawing moved 'events.py' -> 'main.py'; 'events.py' -> Deleted

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from buildings import *
 import menu
 import pygame
 from feature_board import Board
-from events import screen_painter
+import time
 
 pygame.init()
 
@@ -48,6 +48,26 @@ while running:
     # --- FPS ---
 
     clock.tick(FPS)
+
+
+# Def to draw on pygame screen
+def screen_painter(screen, event_type):
+    if event_type == 'menu':
+        screen.fill((230, 230, 230))
+        for button in menu_buttons:
+            button.draw(screen)
+    elif event_type == 'board':
+        screen.fill((245, 245, 245))
+        game_board = Board()
+        game_board.render(screen)
+
+    elif event_type == 'nextturn':
+        global turn
+        screen.fill((245, 245, 245))
+        time.sleep(time_to_next_turn)
+        turn += 1
+        screen_painter(screen, 'board')
+
 
 # --- end ---
 pygame.quit()
