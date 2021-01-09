63479ded27088edb41b757945b3d93ae15df5d01
ksavkin <k.e.savkin@gmail.com>
2021-01-10 01:49:45 +0300

    optimisate proggram

--- board.py
+++ board.py
@@ -2,6 +2,7 @@ import os
 import sys
 from building import *
 import pygame
+from cell import *
 
 
 class Board:
@@ -9,7 +10,7 @@ class Board:
     # consts
     color = pygame.Color(0, 0, 0)
     cell_size_px = 24
-    # screen
+    screen = None
 
     # properties
     cell_count = (1, 1)  # cells count on board by x and y
@@ -17,7 +18,7 @@ class Board:
     buildings = []
 
     # calculating props
-    cells = [[]]  # cells on board
+    cells = None  # cells on board
 
     def __init__(self, screen, cell_count, position_top_left, buildings=[]):
         self.screen = screen
@@ -25,50 +26,28 @@ class Board:
         self.position_top_left = position_top_left
         self.buildings = buildings
 
-        self.cells = [[0] * self.cell_count[0]
-                      for _ in range(self.cell_count[1])]
-
     def render(self):
-        self.render_board()
-        if len(self.buildings) > 0:
-            self.render_building(self.buildings)
-
-    def render_building(self, buildings):
         count = 0
-        for i in range(len(self.cells[0])):
-            for j in range(len(self.cells)):
-                if len(buildings)-1 < count:
-                    return
-
-                # image resize
-                img = pygame.transform.scale(
-                    buildings[count].image, (self.cell_size_px-2, self.cell_size_px-2))
-
-                top_left_x = self.position_top_left[0] + self.cell_size_px * i
-                top_left_y = self.position_top_left[1] + self.cell_size_px * j
-                self.screen.blit(img, [top_left_x, top_left_y])
-
+        self.cells = []
+        for i in range(self.cell_count[0]):
+            row = []
+            for j in range(self.cell_count[1]):
+                building = None
+                if len(self.buildings) - 1 > count:
+                    building = self.buildings[count]
+                cell = Cell(self, (i, j), False, building)
+                row.append(cell)
                 count += 1
+            self.cells.append(row)
 
-    def render_board(self):
-        for i in range(len(self.cells[0])):
-            for j in range(len(self.cells)):
-                top_left_x = self.position_top_left[0] + self.cell_size_px * i
-                top_left_y = self.position_top_left[1] + self.cell_size_px * j
-
-                pygame.draw.rect(
-                    screen,
-                    self.color,
-                    (
-                        (top_left_x, top_left_y),
-                        (self.cell_size_px, self.cell_size_px)
-                    ),
-                    1)
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.click(cell)
 
 
 # example - left board + building board
 # uncomment for test
-""" pygame.init()
+pygame.init()
 
 buildings = []
 station1 = PowerStation("resources/power_station.jpg")
@@ -76,24 +55,38 @@ station2 = MineStation("resources/mine_station.jpg")
 
 buildings.append(station1)
 buildings.append(station2)
-
-size = 1000, 1000
+size = 1152, 720
 screen = pygame.display.set_mode(size)
-screen.fill((255, 0, 0))
-
-board_pole = Board(screen, (6, 3), (100, 50))
-board_buildings = Board(screen, (1, 4), (300, 50), buildings)
+screen.fill((255, 255, 255))
+board_pole = Board(screen, (30, 30), (0, 0))
+board_buildings = Board(screen, (2, 2), (900, 150), buildings)
 
 
 board_pole.render()
+
+board_pole.cells[5][1].set_building(station1)
+board_pole.cells[6][6].set_building(station2)
+
 board_buildings.render()
+
 
 pygame.display.flip()
 
 running = True
-while running:
+"""while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            #board_pole.get_click(event.pos, )
+            #board_pole.cells[2][1].set_building(None)
+            x,y = event.pos
+            for i in range(board_pole.cell_count[0]):
+                for j in range(board_pole.cell_count[1]):
+                    c = board_pole.cells[i][j]
+                    
+                    if c.building is not None and c.building.image.get_rect().collidepoint(event.pos): 
+                        b = c.is_clicked()
+                        board_pole.cells[i][j].set_building(None)
 
-pygame.quit() """
+pygame.quit()"""
---
+++ cell.py
@@ -0,0 +1,63 @@
+import pygame
+
+
+class Cell:
+    # props:
+    board = None
+    is_selected = False
+    building = None
+    position_index = (0, 0)
+
+    # calc props (readonly!)
+    cell_top_left_x_y = (0, 0)
+
+    def __init__(self, board, position_index, is_selected=False, building=None):
+        self.position_index = position_index
+        self.board = board
+        self.is_selected = is_selected
+        self.building = building
+
+        self.cell_top_left_x_y = (
+            board.position_top_left[0] +
+            board.cell_size_px * self.position_index[0],
+            board.position_top_left[1] + board.cell_size_px * self.position_index[1])
+
+        self.render_building(self.building)
+
+    def set_building(self, building):
+        self.building = building
+        self.render_building(self.building)
+
+    def set_selected(self, is_selected):
+        self.is_selected = is_selected
+        self.set_background()
+
+    def render_building(self, building):
+        self.set_background()
+        if building is None:
+            image = pygame.image.load("resources/empty_cell.jpg")
+        else:
+            image = building.image
+
+        # image resize
+        img = pygame.transform.scale(
+            image, (self.board.cell_size_px-2, self.board.cell_size_px-2))
+
+        self.board.screen.blit(
+            img, [self.cell_top_left_x_y[0], self.cell_top_left_x_y[1]])
+        pygame.display.update()
+
+    def set_background(self):
+        background_color = pygame.Color("black")
+        if self.is_selected:
+            background_color = pygame.Color("white")
+        pygame.draw.rect(self.board.screen,
+                         background_color,
+                         (
+                             (self.cell_top_left_x_y[0],
+                              self.cell_top_left_x_y[1]),
+                             (self.board.cell_size_px, self.board.cell_size_px)
+                         ), 1)
+
+    def is_clicked(self):
+        return pygame.mouse.get_pressed()[0] and self.building.image.get_rect().collidepoint(pygame.mouse.get_pos())
